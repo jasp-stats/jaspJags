c1436ed167553d1bda93468e8c1c19f214764094
Don van den Bergh <donvdbergh@hotmail.com>
2025-11-10 16:08:11 +0100

    add a unit test for showing an error for an invalid JAGS model

--- tests/testthat/test-jagsmodule.R
+++ tests/testthat/test-jagsmodule.R
@@ -93,3 +93,122 @@ test_that("Bivariate Scatter Plot matches", {
   testPlot <- results[["state"]][["figures"]][[plotName]][["obj"]]
   jaspTools::expect_equal_plots(testPlot, "bivariate-scatter-plot")
 })
+
+
+test_that("Model errors are captured gracefully", {
+
+  options <- structure(
+    list(
+      .meta = list(initialValues = list(values = list(isRCode = TRUE))),
+      actualExporter = FALSE,
+      aggregatedChains = TRUE,
+      autoCorPlot = FALSE,
+      autoCorPlotLags = 20,
+      autoCorPlotType = "lines",
+      bivariateScatterDiagonalType = "density",
+      bivariateScatterOffDiagonalType = "hexagon",
+      bivariateScatterPlot = FALSE,
+      burnin = 500,
+      chains = 3,
+      colorScheme = "colorblind",
+      customInference = list(
+        list(
+          ciLevel = 0.95,
+          dataSplit = list(types = "unknown", value = ""),
+          ess = TRUE,
+          hdiLevel = 0.95,
+          inferenceCi = FALSE,
+          inferenceCiLevel = 0.95,
+          inferenceCustomHigh = 10,
+          inferenceCustomLow = 1.33,
+          inferenceData = list(types = "unknown", value = ""),
+          inferenceHdi = FALSE,
+          inferenceHdiLevel = 0.95,
+          inferenceManual = TRUE,
+          mean = TRUE,
+          median = TRUE,
+          mode = TRUE,
+          name = "Plot 1",
+          overlayGeomType = "density",
+          overlayHistogramBinWidthType = "sturges",
+          overlayHistogramManualNumberOfBins = 30,
+          parameter = "Cpk",
+          parameterOrder = "orderMean",
+          parameterSubset = "",
+          plotCustomHigh = 10,
+          plotCustomLow = 1.33,
+          plotInterval = "manual",
+          plotsType = "stackedDensity",
+          rhat = TRUE,
+          savageDickey = FALSE,
+          savageDickeyPoint = 0,
+          savageDickeyPosteriorMethod = "samplingPosteriorPoint",
+          savageDickeyPosteriorSamplingType = "normalKernel",
+          savageDickeyPriorHeight = 0,
+          savageDickeyPriorMethod = "sampling",
+          savageDickeySamplingType = "normalKernel",
+          sd = TRUE,
+          shadeIntervalInPlot = TRUE
+        )
+      ),
+      densityPlot = FALSE,
+      deviance = FALSE,
+      exportSamplesFile = "",
+      histogramPlot = FALSE,
+      initialValues = list(
+        list(
+          levels = c("Row 1", "Row 2", "Row 3", "Row 4", "Row 5", "Row 6", "Row 7", "Row 8"),
+          name = "Parameter",
+          values = c("Cpk", "z", "mu", "LSL", "tau", "sigma", "n", "USL")
+        ),
+        list(
+          levels = c("Row 1", "Row 2", "Row 3", "Row 4", "Row 5", "Row 6", "Row 7", "Row 8"),
+          name = "R Code",
+          values = c("...", "...", "...", "...", "...", "...", "...", "...")
+        )
+      ),
+      legend = TRUE,
+      model = list(
+        columns = "Measurement",
+        model = "# The data block can contain fixed values\n# The variable \"Measurement\" has the observations,\n# and these could be added to the data block.\ndata{\n# USL <- 2.4\n#  LSL <- -2.4 \n#  n   <- length(Measurement)\n  z <- 1\n}\nmodel{\n  USL <- 2.4\n  LSL <- -2.4 \n  n   <- length(Measurement)\n\n  # nonconjugate prior distributions informed by the problem at hand:\n  mu    ~ dunif(LSL,USL) \n  sigma ~ dunif(0,USL-LSL)\n  tau <- pow(sigma,-2) # tau is the precision, i.e., 1/variance\n  # Likelihood:\n  for (i in 1:n){\n  Measurement[i] ~ dnorm(mu,tau)\n  } \n  # Propagate to a measure of capability:\n  Cpk <- min((USL-mu)/(3*sigma),(mu-LSL)/(3*sigma))\n}",
+        modelOriginal = "# The data block can contain fixed values\n# The variable \"Measurement\" has the observations,\n# and these could be added to the data block.\ndata{\n# USL <- 2.4\n#  LSL <- -2.4 \n#  n   <- length(Measurement)\n  z <- 1\n}\nmodel{\n  USL <- 2.4\n  LSL <- -2.4 \n  n   <- length(Measurement)\n\n  # nonconjugate prior distributions informed by the problem at hand:\n  mu    ~ dunif(LSL,USL) \n  sigma ~ dunif(0,USL-LSL)\n  tau <- pow(sigma,-2) # tau is the precision, i.e., 1/variance\n  # Likelihood:\n  for (i in 1:n){\n  Measurement[i] ~ dnorm(mu,tau)\n  } \n  # Propagate to a measure of capability:\n  Cpk <- min((USL-mu)/(3*sigma),(mu-LSL)/(3*sigma))\n}",
+        parameters = c("Cpk", "z", "mu", "LSL", "tau", "sigma", "n", "USL")
+      ),
+      monitoredParameters = list(types = list(), value = list()),
+      monitoredParametersShown = list(
+        types = c("unknown", "unknown", "unknown"),
+        value = c("mu", "sigma", "Cpk")
+      ),
+      plotHeight = 320,
+      plotWidth = 480,
+      resultsFor = "allParameters",
+      samples = 2000,
+      seed = 1,
+      setSeed = FALSE,
+      thinning = 1,
+      tracePlot = FALSE,
+      userData = list(
+        list(
+          levels = list(),
+          name = "Parameter",
+          values = list()
+        ),
+        list(
+          levels = list(),
+          name = "R Code",
+          values = list()
+        )
+      )
+    ),
+    analysisName = "JAGS"
+  )
+
+  result <- runAnalysis(options = options, dataset = data.frame(Measurement = rnorm(10)))
+
+  errorMessage <- result$results$mainContainer$collection$mainContainer_mainTable$error$errorMessage
+
+  expect_true(grepl("RUNTIME ERROR:",     errorMessage, fixed = TRUE))
+  expect_true(grepl("Unknown variable n", errorMessage, fixed = TRUE))
+
+})
+
